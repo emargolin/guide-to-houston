0601c0fd17ce814faa05203de78228d34c4922e3
Elena Margolin <emargolin101@gmail.com>
2022-07-16 20:47:25 -0700

    Initial Commit

---
+++ Guide2Houston/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guide2Houston
+//
+//  Created by Elena Margolin on 7/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guide2Houston/Guide2HoustonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guide2HoustonApp.swift
+//  Guide2Houston
+//
+//  Created by Elena Margolin on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Guide2HoustonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
